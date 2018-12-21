da94c8d68d6c2ded9d54af46d8f71707c31cde24
gemul <cybermujahidz@gmail.com>
2018-12-21 18:39:44 +0700

    fix mahasiswa

--- application/controllers/admin/Mahasiswa.php
+++ application/controllers/admin/Mahasiswa.php
@@ -11,13 +11,31 @@ class Mahasiswa extends CI_Controller {
 
 	public function index()
 	{
-        // load view admin/mahasiswa.php
-        $this->load->view("admin/mahasiswa/index");
+		// load view admin/mahasiswa.php
+		$this->load->view("admin/mahasiswa/index");
 	}
 	public function add()
 	{
-        // load view admin/mahasiswa.php
-        $data=Array('judul'=>'Tambah Data Mahasiswa');
-        $this->load->view("admin/mahasiswa/add",$data);
+		// load view admin/mahasiswa.php
+		$data=Array('judul'=>'Tambah Data Mahasiswa');
+		$this->load->view("admin/mahasiswa/add",$data);
+	}
+	public function batchEntry()
+	{
+
+		$this->load->helper('file');
+		if (($handle = fopen("storage/mahasiswa.csv", "r")) !== FALSE) {
+			$row=0;
+		while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+			$num = count($data);
+			echo "<p> $num fields in line $row: <br /></p>\n";
+			$row++;
+			for ($c=0; $c < $num; $c++) {
+				echo $data[$c] . "<br />\n";
+			}
+		}
+		fclose($handle);
+		}
+		// var_dump(read_file("storage/mahasiswa.csv"));
 	}
 }

2ed1f547076834a996f466c2980e97507c70ea12
gemul <cybermujahidz@gmail.com>
2019-01-04 20:18:54 +0700

    menu

--- application/views/admin/_parts/sidebar.php
+++ application/views/admin/_parts/sidebar.php
@@ -6,17 +6,6 @@
             <span>Overview</span>
         </a>
     </li>
-    <li class="nav-item dropdown <?php echo $this->uri->segment(2) == 'products' ? 'active': '' ?>">
-        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
-            aria-expanded="false">
-            <i class="fas fa-fw fa-boxes"></i>
-            <span>Products</span>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-            <a class="dropdown-item" href="<?php echo site_url('admin/products/add') ?>">New Product</a>
-            <a class="dropdown-item" href="<?php echo site_url('admin/products') ?>">List Product</a>
-        </div>
-    </li>
     <li class="nav-item  <?php echo $this->uri->segment(2) == 'tanggungan' ? 'active': '' ?>">
         <a class="nav-link" href="<?php echo site_url('admin/tanggungan') ?>">
             <i class="fas fa-fw fa-clipboard"></i>
@@ -42,9 +31,19 @@
             <i class="fas fa-fw fa-user"></i>
             <span>Users</span></a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Settings</span></a>
+    <li class="nav-item dropdown <?php echo $this->uri->segment(2) == 'products' ? 'active': '' ?>">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
+            <i class="fas fa-fw fa-boxes"></i>
+            <span>Laporan</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <a class="dropdown-item" title="Tampilkan Tanggungan" target="_blank" href="<?php echo site_url('admin/tanggungan/report_all') ?>">
+                <i class="fas fa-fw fa-file"></i>
+                <span>Tanggungan</span></a>
+            <a class="dropdown-item" title="Unduh Tanggungan" target="_blank" href="<?php echo site_url('admin/tanggungan/report_all_csv') ?>">
+                <i class="fas fa-fw fa-download"></i>
+                <span>Tanggungan</span></a>
+        </div>
     </li>
 </ul>
